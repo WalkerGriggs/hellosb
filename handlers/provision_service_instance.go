b58f0f783235efdc3e39acd6902b540a9a211001
WalkerGriggs <walker@walkergriggs.com>
2021-07-27 14:24:24 -0700

    Fleshes out the `provision_service_instance` handler's response codes

--- handlers/provision_service_instance.go
+++ handlers/provision_service_instance.go
@@ -1,7 +1,10 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
+	"reflect"
+	"time"
 
 	"github.com/go-openapi/runtime"
 	"github.com/go-openapi/runtime/middleware"
@@ -25,6 +28,10 @@ func NewProvisionServiceInstanceHandler(store *state.StateStore) service_instanc
 func (impl *provisionServiceInstance) Handle(params service_instances.ServiceInstanceProvisionParams, principal interface{}) middleware.Responder {
 	request := params.Body
 
+	response := &models.ServiceInstanceProvisionResponse{}
+
+	acceptsIncomplete := pointer.BoolDeref(params.AcceptsIncomplete, false)
+
 	instance := &models.ServiceInstanceResource{
 		MaintenanceInfo: request.MaintenanceInfo,
 		PlanID:          pointer.StringDeref(request.PlanID, "planid"),
@@ -33,13 +40,121 @@ func (impl *provisionServiceInstance) Handle(params service_instances.ServiceIns
 	}
 
 	return middleware.ResponderFunc(func(rw http.ResponseWriter, pr runtime.Producer) {
-		err := impl.store.InsertServiceInstance(params.InstanceID, instance)
+		b, err := response.MarshalBinary()
+
+	HAS_ERR:
 		if err != nil {
 			rw.WriteHeader(500)
 			rw.Write([]byte(err.Error()))
 			return
 		}
 
-		rw.WriteHeader(200)
+		existing, err := impl.store.GetServiceInstance(params.InstanceID)
+		if err != nil {
+			goto HAS_ERR
+		}
+
+		if existing != nil {
+			if reflect.DeepEqual(existing.Parameters, instance.Parameters) {
+				if isInstanceProvisioning(existing) && acceptsIncomplete {
+					// MUST be returned if the Service Instance provisioning is in
+					// progress. The operation string MUST match that returned for the
+					// original request. This triggers the Platform to poll the Last
+					// Operation for Service Instances endpoint for operation status. Note
+					// that a re-sent PUT request MUST return a 202 Accepted, not a 200
+					// OK, if the Service Instance is not yet fully provisioned.
+					rw.WriteHeader(202)
+				} else {
+					// SHOULD be returned if the Service Instance already exists, is
+					// fully provisioned, and the requested parameters are identical to
+					// the existing Service Instance. This response is only valid in
+					// synchronous operations.
+					rw.WriteHeader(200)
+				}
+				rw.Write(b)
+			} else {
+
+				// MUST be returned if a Service Instance with the same id already
+				// exists or is being provisioned but with different attributes.
+				rw.WriteHeader(409)
+			}
+			return
+		} else if acceptsIncomplete {
+			// MUST be returned if the Service Instance provisioning is in
+			// progress. The operation string MUST match that returned for the
+			// original request. This triggers the Platform to poll the Last
+			// Operation for Service Instances endpoint for operation status. Note
+			// that a re-sent PUT request MUST return a 202 Accepted, not a 200
+			// OK, if the Service Instance is not yet fully provisioned.
+			rw.WriteHeader(202)
+			return
+		} else {
+			attrs, err := ParseSystemAttributes(instance.Parameters)
+			if err != nil {
+				goto HAS_ERR
+			}
+
+			instance.Metadata = &models.ServiceInstanceMetadata{
+				Attributes: attrs,
+			}
+
+			err = impl.store.InsertServiceInstance(params.InstanceID, instance)
+			if err != nil {
+				goto HAS_ERR
+			}
+
+			// MUST be returned if the Service Instance was provisioned as a result of
+			// this request.
+			rw.WriteHeader(201)
+			rw.Write(b)
+		}
 	})
+}
+
+// isInstanceProvisioning is used to determine if a service is _currently_
+// provisioning. A false response does not indicate that the instance has failed
+// to provision or has already provisioned; it's entirely time-based.
+func isInstanceProvisioning(instance *models.ServiceInstanceResource) bool {
+	attrs := instance.Metadata.Attributes.(map[string]interface{})
+
+	createdAt := attrs["created_at"].(time.Time)
+	duration, ok := attrs["provision_duration"].(time.Duration)
+	if !ok {
+		return false // a service without a provision duration is assumed to be synchronous
+	}
+
+	return createdAt.Add(duration).After(time.Now())
+}
+
+// ParseSystemAttributes is used to parrse any hellosb-specific parameters.
+// These attributes are used to codify behavior that is shared among all service
+// instances, regardless of plans, maintenances etc. Examples include a
+// "created_at" timestamp or "provision_duration".
+func ParseSystemAttributes(parameters interface{}) (interface{}, error) {
+	attrs := map[string]interface{}{
+		"created_at": time.Now(),
+	}
+
+	if parameters != nil {
+		params, ok := parameters.(map[string]interface{}) // making assumptions about params shape
+		if !ok {
+			return nil, fmt.Errorf("Failed to parse parameters. Is it a map?")
+		}
+
+		if val, ok := params["opts_provision_duration"]; ok {
+			str, ok := val.(string)
+			if !ok {
+				return nil, fmt.Errorf("Failed to parse opts_provision_duration. Is it a string?")
+			}
+
+			duration, err := time.ParseDuration(str)
+			if err != nil {
+				return nil, err
+			}
+
+			attrs["provision_duration"] = duration
+		}
+	}
+
+	return attrs, nil
 }
